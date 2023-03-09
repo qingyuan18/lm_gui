be4f299f3f78ab62e96b6ea087a440f0b8b5e1e0
qingyuan18 <qingyuan18@gmail.com>
2023-03-09 18:06:11 +0800

    commit

--- sd_model.py
+++ sd_model.py
@@ -1,10 +1,56 @@
 from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline, DPMSolverMultistepScheduler
-import gradio as gr
 import torch
 from PIL import Image
+import boto3
+
+s3_resource = boto3.resource('s3')
+sm_endpoint_1 = 'AIGC-Quick-Kit-8f46c6b9-be46-48a0-b7b6-6c01dacedcd6'
+
+def get_bucket_and_key(s3uri):
+    pos = s3uri.find('/', 5)
+    bucket = s3uri[5 : pos]
+    key = s3uri[pos + 1 : ]
+    return bucket, key
+
+
+def get_images(response):
+    imags=[]
+    try:
+        predictions = response['result']
+        print(predictions)
+        for prediction in predictions:
+            bucket, key = get_bucket_and_key(prediction)
+            obj = s3_resource.Object(bucket, key)
+            bytes = obj.get()['Body'].read()
+            image = Image.open(io.BytesIO(bytes))
+            imags.append(image)
+    except Exception as e:
+        traceback.print_exc()
+        print(e)
+    print(f"Time taken: {time.time() - start}s")
+    return images
+
+def sm_sd_inference(sm_endpoint=sm_endpoint_1, 
+                    prompt, guidance, steps, width=512, 
+                    height=512, seed=0, img=None, 
+                    strength=0.5, neg_prompt="", 
+                    auto_prefix=False, generator, 
+                    inf_type='text_to_image'):
+    payload = json.dumps(test_data.tolist())
+    sagemaker = boto3.client('sagemaker-runtime')
+    response = sagemaker.invoke_endpoint(
+        EndpointName=sm_endpoint,
+        ContentType='application/json',
+        Body=payload
+    )
+    result = response['Body'].read()
+    result = json.loads(result)
+    get_images(result)
+
 
 model_id = 'andite/anything-v4.0'
 prefix = ''
+sm_endpoint='AIGC-Quick-Kit-8f46c6b9-be46-48a0-b7b6-6c01dacedcd6'
      
 scheduler = DPMSolverMultistepScheduler.from_pretrained(model_id, subfolder="scheduler")
 
@@ -22,6 +68,10 @@ if torch.cuda.is_available():
   pipe = pipe.to("cuda")
   pipe_i2i = pipe_i2i.to("cuda")
 
+    
+def sm_inference(sm_endpoint, prompt, guidance, steps, width=512, height=512, seed=0, img=None, strength=0.5, neg_prompt="", auto_prefix=False):
+    pass
+    
 def error_str(error, title="Error"):
     return f"""#### {title}
             {error}"""  if error else ""
