3713dc54aa8a85656b3e32f304b7b1c99d30fdb3
qingyuan18 <qingyuan18@gmail.com>
2023-03-12 15:53:19 +0800

    commit

--- app_streamlit.py
+++ app_streamlit.py
@@ -56,6 +56,7 @@ sm_endpoint_option = st.sidebar.selectbox("Endpoints in SageMaker", sm_endpoint_
 if st.sidebar.button('refresh sm endpoints'):
     new_options = list_sm_endpoints()
     sm_endpoint_opts=new_options
+    st.sidebar.info("SageMaker endpoints updated!")
     #sm_endpoint_option = st.sidebar.selectbox("Endpoints in SageMaker", new_options)
 
 
