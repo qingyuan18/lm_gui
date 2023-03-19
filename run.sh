f72d97d0551f1273d411e0891ed0d8fe0933a1fc
qingyuan18 <qingyuan18@gmail.com>
2023-03-19 14:26:34 +0800

    commit

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [[ "$1" == "start" ]]; then
   echo "Starting gui playground..."
-  nohup streamlit run --server.maxUploadSize=5024 --server.port 8501 ./app_streamlit_2.py &
+  nohup streamlit run --server.maxUploadSize=5024  --server.maxMessageSize 2048 --server.port 8501 ./app_streamlit_2.py &
 elif [[ "$1" == "stop" ]]; then
   echo "Stopping gui playground..."
   ps -ef|grep -v "grep"|grep -i streamlit|awk '{print $2}'|while read line
